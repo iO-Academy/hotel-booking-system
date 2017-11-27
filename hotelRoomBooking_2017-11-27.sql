48d002679f7be827d46a75f2f4c42e56e8b76aa0
Ben Long <bennlongg@gmail.com>
2017-11-27 15:06:41 +0000

    name change

--- hotelRoomBooking_2017-11-27.sql
+++ hotelRoomBooking_2017-11-27.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.6.37)
 # Database: hotelRoomBooking
-# Generation Time: 2017-11-27 15:00:47 +0000
+# Generation Time: 2017-11-27 15:06:14 +0000
 # ************************************************************
 
 
@@ -34,6 +34,35 @@ CREATE TABLE `roomImages` (
   CONSTRAINT `roomTypeFK` FOREIGN KEY (`roomType`) REFERENCES `roomTypes` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+
+# Dump of table roomTypes
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `roomTypes`;
+
+CREATE TABLE `roomTypes` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(8) NOT NULL DEFAULT '',
+  `pricePerNight` float NOT NULL,
+  `numberOfAdults` int(11) NOT NULL,
+  `hasCot` tinyint(1) NOT NULL,
+  `minStay` int(11) NOT NULL,
+  `numberInHotel` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `roomTypes` WRITE;
+/*!40000 ALTER TABLE `roomTypes` DISABLE KEYS */;
+
+INSERT INTO `roomTypes` (`id`, `name`, `pricePerNight`, `numberOfAdults`, `hasCot`, `minStay`, `numberInHotel`)
+VALUES
+	(1,'Standard',60,2,0,2,6),
+	(2,'Family',100,4,1,2,2),
+	(3,'VIP',400,10,0,5,2);
+
+/*!40000 ALTER TABLE `roomTypes` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 
