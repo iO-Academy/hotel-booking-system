1d9458d07bfde2e028151671125936b501d40536
Ruth Waterfield <ruth.waterfield@mayden.co.uk>
2017-11-27 13:38:20 +0000

    started routing

--- src/routes.php
+++ src/routes.php
@@ -5,10 +5,20 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+$app->get('/', function(Request $request, Response $response, array $args) {
+   $this->logger->info("Request to display page");
 
-    // Render index view
+   //Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+
+$app->get('/rooms', function (Request $request, Response $response, array $args) {
+    $this->logger->info("Getting room types info");
+
+    $this->db;
+    //instantiate query class and get JSON with room type info back.
+
+   //return JSON with room type info
+   return $this->renderer->render($response, 'index.phtml', $args);
 });
