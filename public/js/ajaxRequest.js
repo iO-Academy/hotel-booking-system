07e3c004af33cf89c64cf09d21eb4e96168056b5
Peter Kanev <pdkanew1@gmail.com>
2017-11-28 09:40:43 +0000

    added docblocs to the functions

--- public/js/ajaxRequest.js
+++ public/js/ajaxRequest.js
@@ -1,3 +1,7 @@
+/**
+ * function to make ajax requests in order to populate the landing page with information for the rooms
+ * @return {boolean}
+ */
 function makeRequest() {
     httpRequest = new XMLHttpRequest()
 
@@ -9,6 +13,11 @@ function makeRequest() {
     httpRequest.open('GET', '../../src/routes.php', true)
     httpRequest.send()
 }
+
+/**
+ * function to check if there is response from the backend and handle any failures
+ * @return {string}
+ */
  function alertContents() {
      if (httpRequest.readyState === XMLHttpRequest.DONE) {
          if (httpRequest.status === 200) {
