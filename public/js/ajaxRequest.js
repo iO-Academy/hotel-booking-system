5561d48ed7a9dc007fcaabd1296102fab0e46105
Peter Kanev <pdkanew1@gmail.com>
2017-11-28 09:56:20 +0000

    added docblocs and fixed the previous problems with the files

--- public/js/ajaxRequest.js
+++ public/js/ajaxRequest.js
@@ -1,3 +1,7 @@
+/**
+ * function to make ajax requests in order to populate the landing page with information for the rooms
+ * @return {boolean}
+ */
 function makeRequest() {
     httpRequest = new XMLHttpRequest()
 
@@ -9,15 +13,20 @@ function makeRequest() {
     httpRequest.open('GET', '../../src/routes.php', true)
     httpRequest.send()
 }
- function alertContents() {
-     if (httpRequest.readyState === XMLHttpRequest.DONE) {
-         if (httpRequest.status === 200) {
-             return httpRequest.responseText
-         } else {
-             console.log('Sorry, our badger eat the cables once again');
-         }
-     }
- }
+
+/**
+ * function to check if there is response from the backend and handle any failures
+ * @return {string}
+ */
+function alertContents() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+            return httpRequest.responseText
+        } else {
+            console.log('Sorry, our badger eat the cables once again');
+        }
+    }
+}
 
 window.addEventListener('DOMContentLoaded', function() {
     fetch('/rooms', {
